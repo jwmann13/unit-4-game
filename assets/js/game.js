35c1272ddc6b62349248a8d9bf2fd6ce905577d1
Jeffrey Mann <jwmann22@gmail.com>
2019-09-15 15:22:07 -0400

    consolidated functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,71 +3,67 @@ $(document).ready(function () {
     let wins = 0;
     let losses = 0;
     let userScore = 0;
-    let adders = [];
+    let adders = randomAdderSet();
 
-    let targetDiv = $('#target');
-    let winsDiv = $('#wins');
-    for (let i = 0; i < 4; i++) {
-        let img = $("<img>");
-        img.addClass('img-responsive h-auto m-0');
-        img.attr('src', `assets/images/crystal${i}.jpeg`);
-        img.css('max-width', '100%');
-        $('#crystal' + i).append(img);
-    }
+    initWrite();
 
-    for (let i = 0; i < 4; i++) {
-        let temp = Math.floor(11 * (Math.random())) + 1;
-        if (!adders.includes(temp)) {
-            adders.push(temp);
-        } else {
-            i--;
+    $('#buttons>button').on('click', function() {
+        buttonSound($(this).data("index"));
+        userScore += adders[$(this).data("index")];
+        $('#user-score').text(userScore);
+        checkWinState(userScore);
+    });
+
+    function checkWinState(score) {
+        if (score == target) {
+            // $('#user-score:last-child').detach();
+            let winMsg = $('<div>');
+            winMsg.addClass('display-3 col-8');
+            winMsg.text('You Win!')
+            $('#score-win').append(winMsg);
+            wins++;
+            reset();
+        } else if (score > target) {
+            // $('#score-win:last-child').detach();
+            let winMsg = $('<div>');
+            winMsg.addClass('display-3 col-8');
+            winMsg.text('You Lost!')
+            $('#score-win').append(winMsg);
+            losses++;
+            reset();
         }
-
     }
-    console.log(adders);
 
+    function reset() {
+        target = (Math.floor(100 * Math.random())) + 20;
+        userScore = 0;
+        adders = randomAdderSet();
+        initWrite();
+    }
 
+    function initWrite() {
+        $('#target').text(target);
+        $('#wins').text(`Wins: ${wins} Losses: ${losses}`);
+        $('#user-score').text(userScore);
+    }
 
-    targetDiv.text(target);
-    winsDiv.text(`Wins: ${wins} Losses: ${losses}`)
+    function randomAdderSet() {
+        let arr = [];
+        for (let i = 0; i < 4; i++) {
+            let temp = Math.floor(11 * (Math.random())) + 1;
+            if (!arr.includes(temp)) {
+                arr.push(temp);
+            } else {
+                i--;
+            }
+        }
+        return arr;
+    }
 
-    $('#crystal0').on('click', function () {
-        let sound = document.createElement('audio');
-        sound.setAttribute('src', 'assets/audio/sound0.wav');
-        sound.pause();
-        sound.currentTime = 0;
-        $(this).append(sound);
+    function buttonSound(index) {
+        let sound = new Audio(`assets/audio/sound${index}.wav`);
+        // console.log(sound);
+        sound.volume = 0.5;
         sound.play();
-    });
-
-    $('#crystal1').on('click', function () {
-        let sound = document.createElement('audio');
-        sound.setAttribute('src', 'assets/audio/sound1.wav');
-        sound.pause();
-        sound.currentTime = 0;
-        $(this).append(sound);
-        sound.play();
-    });
-
-    $('#crystal2').on('click', function () {
-        let sound = document.createElement('audio');
-        sound.setAttribute('src', 'assets/audio/sound2.wav');
-        sound.pause();
-        sound.currentTime = 0;
-        $(this).append(sound);
-        sound.play();
-    });
-
-    $('#crystal3').on('click', function () {
-        let sound = document.createElement('audio');
-        sound.setAttribute('src', 'assets/audio/sound3.wav');
-        sound.pause();
-        sound.currentTime = 0;
-        $(this).append(sound);
-        sound.play();
-    });
-
-
-
-
-})
+    }
+});
